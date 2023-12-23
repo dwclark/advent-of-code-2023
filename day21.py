7bcd08372ac8d29b76af68cb64eace6724282bfa
David Clark <me@nobody.com>
2023-12-23 09:46:05 -0600

    Finished day 21 part 2

--- day21.py
+++ day21.py
@@ -9,17 +9,23 @@ def find_start(graph):
               graph[row_index] = row[0:col_index] + '.' + row[col_index+1:]
               return ret
 
-def is_legal(graph, at):
+def is_legal_1(graph, at):
     row, col = at
     return (row >= 0 and col >= 0 and row < len(graph)
             and col < len(graph[0]) and graph[row][col] != '#')
 
-def total_reached(graph, start, num):
+def is_legal_2(graph, at):
+    raw_row, raw_col = at
+    row = raw_row % len(graph)
+    col = raw_col % len(graph)
+    return graph[row][col] != '#'
+
+def total_reached(graph, start, num, is_legal):
     q = deque()
     q.append((start, 0))
     reached = set()
     cache = {}
-
+    
     def add_queue(pos, steps):
         sub = cache.get(steps)
         if not sub:
@@ -37,11 +43,25 @@ def total_reached(graph, start, num):
         else:
             row, col = pos
             for next_pos in [(row+1, col), (row-1, col), (row, col+1), (row, col-1)]:
-                if is_legal(graph, next_pos):
+                if is_legal(graph, next_pos) and not next_pos in reached:
                     add_queue(next_pos, steps+1)
 
     return len(reached)
 
+def part_2(graph, start, num):
+    remainder = num % len(graph)
+
+    v1 = total_reached(graph, start, remainder, is_legal_2)
+    v2 = total_reached(graph, start, remainder + len(graph), is_legal_2)
+    v3 = total_reached(graph, start, remainder + (2 * len(graph)), is_legal_2)
+
+    a = (v1 - 2*v2 + v3) / 2
+    b = (-3*v1 + 4*v2 - v3) / 2
+    c = v1
+    n = int(num / len(graph))
+    return int((a * n * n) + (b * n) + c)
+    
 graph = non_blank_lines('input/day21.txt')
 start = find_start(graph)
-print(total_reached(graph, start, 64))
+print_assert("Part 1:", total_reached(graph, start, 64, is_legal_1), 3600)
+print_assert("Part 2:", part_2(graph, start, 26501365), 599763113936220)
